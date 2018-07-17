e82cb2af52bef7e668a8a1f1e43689c18247aef6
gupta <39546074+guptagrandhi@users.noreply.github.com>
2018-07-17 11:48:41 +0530

    Add files via upload

---
+++ Javascript/24.js
@@ -0,0 +1,10 @@
+
+<script type="text/javascript">
+<!--
+function sayHello() {
+alert("Hello World")
+<input type="button" onclick="sayHello()" value="Say Hello" />
+document.write(function);	
+}
+//-->
+</script>
